53e2c5b561ee866c8e1633b8b0bc444df233d4ff
sujanchandrar <roys33664@gmail.com>
2024-02-19 19:43:24 +0600

    apply button function add

--- Script/script.js
+++ Script/script.js
@@ -5,17 +5,19 @@ let count = 0;
 for(const btns of allBtn){
     btns.addEventListener('click', function(event) {
 
-        console.log(event.target.parentNode.childNodes)
-
         const setName = event.target.parentNode.childNodes[3].innerText;
-        console.log(setName);
+        
        count = count + 1;
        const setForti = document.getElementById('setforti').innerText;
        const setFortiConvertnmbr = parseInt(setForti);
        console.log(typeof setFortiConvertnmbr);
 
-      
+      if(count === 4 ){
+
+        document.getElementById('coupon-field').classList.remove('hidden');
+      }
        if(count > 4){
+        document.getElementById('apply-btn').classList.remove('hidden')
         alert('you cannnot buy any more seats');
         return;
        }
@@ -23,7 +25,8 @@ for(const btns of allBtn){
        btns.classList.add('bg-green-400');
        document.getElementById('setforti').innerText = setFortiConvertnmbr - 1;
         document.getElementById('set').innerText = count;
-
+        event.target.setAttribute("disabled", "true");
+        event.target.style.backgroundColor = 'green';
         // ------------
 
         const ul = document.getElementById('li-container');
@@ -40,46 +43,36 @@ for(const btns of allBtn){
         li1.appendChild(p3);
         ul.appendChild(li1);
 
-        // ------------
-
-        // liElement()
-
         const totalPrice = document.getElementById('total-rice').innerText;
         const totalPriceConvert = parseInt(totalPrice);
         document.getElementById('total-rice').innerText = totalPriceConvert + 550;
 
         const grandPrice = document.getElementById('grand-total').innerText;
-        const grandPriceConvert = parseInt(totalPrice);
+        const grandPriceConvert = parseInt(grandPrice);
         document.getElementById('grand-total').innerText = grandPriceConvert + 550;
     })
 
     
 };
 
-function liElement() {
-
-    const ul = document.getElementById('li-container');
+function apply() {
+    const inputValue = document.getElementById('inpt-field').value;
+    const grndtotal = document.getElementById('grand-total').innerText;
+    const convertGrandTotal = parseInt(grndtotal);
     
+    if(inputValue === 'NEW15'){
+        const value = convertGrandTotal * 15 / 100;
+        const comisionp = convertGrandTotal - value;
+        document.getElementById('grand-total').innerText = comisionp;
 
-    const li1 = document.createElement('li');
-    li1.appendChild(p1);
-    const li2 = document.createElement('li');
-    const li3 = document.createElement('li');
-    const li4 = document.createElement('li');
+    }
+    else if(inputValue === 'Couple 20'){
+        const value = convertGrandTotal * 20 / 100;
+        const comisionp = convertGrandTotal - value;
+        document.getElementById('grand-total').innerText = comisionp;
 
-    const p1 = document.createElement('p');
-    p1.innerText = 'c2';
-    const p2 = document.createElement('p');
-    p2.innerText = 'Emergency';
-    const p3 = document.createElement('p');
-    p3.innerText = '550';
-
-    ul.appendChild(li1);
+    }
+    else{
+        document.getElementById('grand-total').innerText;
+    }
 }
-
-
-// function seatsCount(id, value){
-//     const sets = document.getElementById(id).innerText;
-//     const convertSets = parseInt(sets);
-
-// }
